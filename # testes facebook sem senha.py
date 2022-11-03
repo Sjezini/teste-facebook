8415b3a03b6b765bac8c6f5bb9bd0ca5b37ac927
Sjezini <jezini6@gmail.com>
2022-11-03 14:16:40 -0300

    Create # testes facebook sem senha.py

---
+++ # testes facebook sem senha.py
@@ -0,0 +1,19 @@
+# testes facebook sem senha
+import time
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+servico = Service(GeckoDriverManager().install())
+
+navegador = webdriver.Firefox(service=servico)
+
+navegador.get("https://pt-br.facebook.com/")
+
+
+navegador.find_element(
+    'xpath', '//*[@id="email"]').send_keys("jezinipsn@gmail.com")
+navegador.find_element(
+    'xpath', '//*[@id="pass"]').send_keys("")
+navegador.find_element(
+    'xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click()
