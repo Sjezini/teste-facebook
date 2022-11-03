714f4b63cdca46eb82c2ebae2cb6deaf9db84ba3
Sjezini <jezini6@gmail.com>
2022-11-03 14:16:41 -0300

    Create # testes facebook sem email.py

---
+++ # testes facebook sem email.py
@@ -0,0 +1,18 @@
+# testes facebook sem email
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+servico = Service(GeckoDriverManager().install())
+
+navegador = webdriver.Firefox(service=servico)
+
+navegador.get("https://pt-br.facebook.com/")
+
+
+navegador.find_element(
+    'xpath', '//*[@id="email"]').send_keys("")
+navegador.find_element('xpath', '//*[@id="pass"]').send_keys("teste2134@")
+navegador.find_element(
+    'xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click()
